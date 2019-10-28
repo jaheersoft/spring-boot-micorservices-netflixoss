d92a1ac8593391743cb781f25dcf2a38e8110f2b
Jaheer <Jaheer@LAPTOP-RMO3725H>
2019-10-28 16:44:05 +0530

    turbine stream service project using rabbitmq turnine

---
+++ turbine-stream-service/src/test/java/com/expenses/turbine/TurbineStreamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenses.turbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurbineStreamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
